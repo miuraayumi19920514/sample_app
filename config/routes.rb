a06b2cedaf9938f3c26de4a42a6ad241c73cf30d
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-08 13:02:52 +0000

    [add]アクション、ルーティングを変更しインデックスビューを記述。リストの一覧画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'new' => 'lists#new'
   post 'lists' => 'lists#create'
-  get 'lists/index'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'edit' => 'lists#edit'
   get 'top' => 'homes#top'
