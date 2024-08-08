c5adc8072cd4d10152189c4da66a7dc0efe15dde
miuraayumi19920514 <174306160+miuraayumi19920514@users.noreply.github.com>
2024-08-08 10:22:09 +0000

    [add]newビューページを作成　Listテーブルを反映

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+  get 'new' => 'lists#new'
+  post 'lists' => 'lists#create'
   get 'lists/index'
   get 'lists/show'
   get 'edit' => 'lists#edit'
